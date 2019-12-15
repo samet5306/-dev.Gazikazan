2f2317f6a466274cbc5b31de760bc22911591ba3
samed balcı <ssamet0662@hotmail.com>
2019-12-15 19:30:39 +0300

    değişikli

--- ödev.Gazikazan/Program.cs
+++ ödev.Gazikazan/Program.cs
@@ -24,6 +24,7 @@ namespace ödev.Gazikazan
                 int sayı = int.Parse(Console.ReadLine());
                 if(i==3 && rasgele==sayı)
                 {
+                    Console.WriteLine("tebrikler 1 seferde sayıyı buldunuz");
                     Console.WriteLine($"100 puan aldınız :");
                     puan = 100 / 1;
                     Console.WriteLine("ortalamanız={0}", puan);
@@ -31,6 +32,7 @@ namespace ödev.Gazikazan
                 }
                 else if(i==2 && rasgele == sayı)
                 {
+                    Console.WriteLine("tebrikler 2 seferde sayıyı buldunuz");
                     Console.WriteLine($"70 puan idare eder:");
                     puan = 70 / 2;
                     Console.WriteLine("ortalamanız={0}",puan);
@@ -39,17 +41,13 @@ namespace ödev.Gazikazan
                 }
                 else if(i==0 && rasgele == sayı)
                 {
+                    Console.WriteLine("tebrikler 3 seferde sayıyı buldunuz");
                     Console.WriteLine($"50 puan eh işte:");
                     puan = 50 / 3;
                     Console.WriteLine("ortalamanız={0}",puan);
                     break;
                 }
-                else if (sayı == rasgele)
-                {
-                    Console.WriteLine("Tebrikler Doğru Sayıyı Buldunuz");
-                   
-                    break;
-                }
+                
 
             }
             while (true)
